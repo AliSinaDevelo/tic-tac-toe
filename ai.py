7152ae50f59a8deb2374f226ae18f4dc5fa8b16e
AliSinaDevelo <alisinakarimi.2003@gmail.com>
2025-02-02 02:32:37 +0100

    define different components

---
+++ ai.py
@@ -0,0 +1,14 @@
+import random
+import copy
+
+def get_easy_move(board, available_moves):
+    return random.choice(available_moves)
+
+def get_medium_move(board, ai_marker, opponent_marker, available_moves):
+    pass
+
+def get_hard_move(board, ai_marker, opponent_marker):
+    pass
+
+def minimax(board, depth, is_maximizing, ai_marker, opponent_marker):
+    pass
